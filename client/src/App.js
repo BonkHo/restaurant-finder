f80854b3d1aa0292a2b0fce2f41dc9ac7792a959
John Bae <john21225@gmail.com>
2021-12-08 15:08:51 -0800

    Routes for the client are setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// Components
+import HomePage from "./routes/HomePage";
+import DetailsPage from "./routes/DetailsPage";
+import UpdatePage from "./routes/UpdatePage";
 
 function App() {
-	return <div className="App">Sup</div>;
+	return (
+		<div>
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<HomePage />} />
+					<Route
+						exact
+						path="/restaurants/:id/update"
+						element={<UpdatePage />}
+					/>
+					<Route exact path="/restuarants/:id" element={<DetailsPage />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
