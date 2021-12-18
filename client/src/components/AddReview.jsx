28fbeef5b47f00a67317a0f316ca6ef6e0a8448c
John Bae <john21225@gmail.com>
2021-12-17 16:29:17 -0800

    Created states for each form

--- client/src/components/AddReview.jsx
+++ client/src/components/AddReview.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddReview = () => {
+	const [name, setName] = useState("");
+	const [rating, setRating] = useState("Rating");
+	const [review, setReview] = useState("");
+
 	return (
 		<div>
 			<form action="">
-				<div className="form-row">
-					<div className="form-group col-8">
+				<div className="row mb-3">
+					<div className="form-group col">
 						<label htmlFor="name">Name</label>
 						<input
 							id="name"
-							placeholder="name"
+							placeholder="Name"
 							type="text"
 							className="form-control"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
 						/>
 					</div>
-					<div className="form-group col-4">
+					<div className="form-group col">
 						<label htmlFor="rating">Rating</label>
-						<select id="rating" className="custom-select">
+						<select
+							value={rating}
+							onChange={(e) => setRating(e.target.value)}
+							id="rating"
+							className="form-select"
+						>
 							<option disabled>Rating</option>
 							<option value="1">1</option>
 							<option value="2">2</option>
@@ -26,9 +37,14 @@ const AddReview = () => {
 						</select>
 					</div>
 				</div>
-				<div className="form-group">
+				<div className="form-group mb-3">
 					<label htmlFor="Review">Review</label>
-					<textarea id="Review" className="form-control"></textarea>
+					<textarea
+						id="Review"
+						className="form-control"
+						value={review}
+						onChange={(e) => setReview(e.target.value)}
+					></textarea>
 				</div>
 				<button className="btn btn-primary">Submit</button>
 			</form>
